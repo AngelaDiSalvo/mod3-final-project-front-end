41db2e7769eb7bbf9915ffc4200de9b33aec93a4
github username <github email address>
2018-09-11 17:59:36 -0500

    blahhhh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 let map;
 
 function initMap() {
-
-  fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=houston', {
+  fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=houston&limit=10', {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -14,40 +13,43 @@ function initMap() {
 
   function ratingGreaterThanThree(data) {
     const highRatings = data.businesses.filter(business => {
-      // debugger
       return business.rating > 3
     })
-    debugger
 
     let locations = []
-
+    let ids = []
+    let busName = []
     highRatings.forEach(function(business, i) {
-      //grab the name
-      // debugger
+
       let nestedlocations = []
       nestedlocations.push(business.name)
       nestedlocations.push(business.coordinates.latitude)
       nestedlocations.push(business.coordinates.longitude)
       nestedlocations.push(++i)
       locations.push(nestedlocations)
-
-      // create local const for the event listener to create a function to post
-      // nestedlocations.push(business.image_url)
-
+      ids.push(business.id)
+      busName.push(business.name)
     })
 
-
+    let searchCity = "miami"
+    let geocoder =  new google.maps.Geocoder()
+    geocoder.geocode( { 'address': searchCity}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        const lat = results[0].geometry.location.lat()
+        const lng = results[0].geometry.location.lng()
+      } else {
+        alert("Something got wrong " + status)
+      }
+    })
 
     let map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
-      center: new google.maps.LatLng(29.7590441, -95.3635909),
+      center: new google.maps.LatLng(lat, lng),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
     let infowindow = new google.maps.InfoWindow();
-
     let marker, i;
-
     for (i = 0; i < locations.length; i++) {
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
@@ -58,6 +60,8 @@ function initMap() {
         return function() {
           infowindow.setContent(locations[i][0]);
           infowindow.open(map, marker);
+          const businessName = infowindow.content
+          passSongName(businessName)
         }
       })(marker, i));
     }
@@ -65,22 +69,47 @@ function initMap() {
   }
 }
 
-// function initSong () {
-//
-//   fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=coffee&location=houston', {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Authorization": `Bearer ${yelpApiKey}`
-//       }
+// function initSong(ids) {
+//   let commentsArray=[]
+//   const fetches = ids.map(id => {
+//     return fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id + "/reviews", {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Authorization": `Bearer ${yelpApiKey}`
+//         }
+//       })
+//       .then(res => res.json())
 //     })
-//     .then(res => res.json())
-//     .then(comments)
+//     reviewArray = Promise.all(fetches).then(comments)
 //
-//     function comments(data) {
-//       console.log(data)
+//     function comments(reviewArray) {
+//       const reviewss = reviewArray.map(review => {
+//         return review.reviews
+//       })
+//       const blaj = reviewss.map(review => {
+//         review.map(revie => {
+//           revie.text
+//         })
+//       })
 //
+//
+//       // data.reviews.forEach(review => {
+//       //   commentsArray.push(review.text)
+//       // })
+//
+//       let reserve = ["I", "to"]
+//       let result
+//       reserve.forEach(rWord => {
+//         commentsArray.join().split(" ").filter(cWord => {
+//           cWord !== rWord
+//         })
+//       })
 //     }
-//
-//
+//     console.log(commentsArray)
 // }
+
+function passSongName(business) {
+  debugger
+
+}
